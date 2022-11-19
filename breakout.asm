ecfc88ecc068eb0521e9d3588f1a1485b867cf79
Matthew <mz.liu@mail.utoronto.ca>
2022-11-18 21:19:25 -0500

    started work on the line drawing functions

--- breakout.asm
+++ breakout.asm
@@ -1,23 +1,27 @@
 ################ CSC258H1F Fall 2022 Assembly Final Project ##################
 # This file contains our implementation of Breakout.
 #
-# Student 1: Name, Student Number
+# Student 1: Matthew Liu, Student Number
 # Student 2: Name, Student Number
 ######################## Bitmap Display Configuration ########################
-# - Unit width in pixels:       TODO
-# - Unit height in pixels:      TODO
-# - Display width in pixels:    TODO
-# - Display height in pixels:   TODO
+# - Unit width in pixels:       8
+# - Unit height in pixels:      8
+# - Display width in pixels:    256
+# - Display height in pixels:   256
 # - Base Address for Display:   0x10008000 ($gp)
 ##############################################################################
 
-    .data
+.data
 ##############################################################################
 # Immutable Data
 ##############################################################################
 # The address of the bitmap display. Don't forget to connect it!
 ADDR_DSPL:
     .word 0x10008000
+MY_COLOURS:
+	.word	0xff0000    # red
+	.word	0x00ff00    # green
+	.word	0x0000ff    # blue
 # The address of the keyboard. Don't forget to connect it!
 ADDR_KBRD:
     .word 0xffff0000
@@ -29,12 +33,33 @@ ADDR_KBRD:
 ##############################################################################
 # Code
 ##############################################################################
-	.text
-	.globl main
+.text
+.globl main
 
 	# Run the Brick Breaker game.
 main:
     # Initialize the game
+    
+
+# draw horizontal line function ($a0 - start_address, $a1 - colour, $t9 - unit increment, $t8 return - void) 
+draw_line:
+    li $t0, 32  # loop runs 32 times
+    li $t1, 0   # iteration num
+
+draw_line_loop:
+    slt $t2, $t1, $t0
+    beq $t2, $0, end_draw_line
+
+        sw $a1, 0($a0)
+        addi $a0, $a0, 4
+    
+    addi $t1, $t1, 1
+    b draw_line_loop 
+
+draw_h_line_end:
+    jr $ra 
+
+# draw vertical line function ($a0 - start_address, $a1 - colour, return - void) 
 
 game_loop:
 	# 1a. Check if key has been pressed
