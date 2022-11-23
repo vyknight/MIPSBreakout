ceff587c70fc21baf625bdeb81fc223011043477
Matthew <mz.liu@mail.utoronto.ca>
2022-11-22 22:14:01 -0500

    Made brick drawing functional

--- breakout.asm
+++ breakout.asm
@@ -74,27 +74,28 @@ main:
     
     # draw bricks
     # first line
-    
+
 draw_bricks:
     li $s0, 8	# iteration max
     li $s1, 0   # iteration num
-    la $s2, ADDR_DSPL
-    lw $s2, 0($s2)
+    
 draw_bricks_loop:
     slt $s2, $s1, $s0
     beq $s2, $0, draw_bricks_end
-    
-    	la $a1, MY_COLOURS
-    	lw $a1, 0($a1)
     	
-    	add $a0, $s2, $0
+    	# using iteration to get next address
+    	sll $a0, $s1, 2	# multiply by 16
+    	li $a1, 4
+    	jal get_address
 	
+	move $a0, $v0
+	la $a1, MY_COLOURS
+    	lw $a1, 0($a1)
 	li $a2, 4	# loading here so we can use the value for multi
     	li $a3, 3
     	
-    jal draw_line
+    	jal draw_line
     addi $s1, $s1, 1
-    addi $s2, $s2, 16
     b draw_bricks_loop
     
 draw_bricks_end:
@@ -102,6 +103,21 @@ draw_bricks_end:
     # go to game loop
     b game_loop
     
+# FUNCTIONS ################
+
+b game_loop # if main ever gets to this point we don't want to start running functions, send to game loop
+
+# FUNCTION get memory address given x and y coordinates ($a0 - x coord, $a1 - y coord, $v0 - return)
+get_address:
+    sll $a0, $a0, 2	# multiply by 4
+    sll $a1, $a1, 7	# multiply by 128
+    
+    la $v0, ADDR_DSPL
+    lw $v0, 0($v0)
+    add $v0, $a0, $v0 
+    add $v0, $a1, $v0
+    
+    jr $ra
 
 # FUNCTION draw line across screen ($a0 - start_address, $a1 - colour, 
 					#$a2 memory increment (4 for horizontal, 128 for vertical), 
